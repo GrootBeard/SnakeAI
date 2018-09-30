904609ef2aad6717d8db7a93e04c2592b03f269f
Lucas <lucasvdtogt@gmail.com>
2018-09-30 12:26:46 +0200

    Added scaling for graphics to snake tester script

--- src/snake_tester.py
+++ src/snake_tester.py
@@ -1,13 +1,16 @@
 # from generation import Generation
 from snake import AutonomousSnake
 import pygame
+import sys
 
 
 def play(_snake):
     pygame.init()
 
-    display_width = 800
-    display_height = 576
+    SCALE = 0.5
+
+    display_width = int(800 * SCALE)
+    display_height = int(800 * SCALE)
 
     display = pygame.display.set_mode((display_width, display_height))
     pygame.display.set_caption("Snek")
@@ -15,12 +18,15 @@ def play(_snake):
     body_img = pygame.image.load("assets/snekpart.png")
     head_img = pygame.image.load("assets/snekhead.png")
     food_img = pygame.image.load("assets/snekfood.png")
+    body_img = pygame.transform.scale(body_img, (int(32 * SCALE), int(32 * SCALE)))
+    head_img = pygame.transform.scale(head_img, (int(32 * SCALE), int(32 * SCALE)))
+    food_img = pygame.transform.scale(food_img, (int(32 * SCALE), int(32 * SCALE)))
 
     clock = pygame.time.Clock()
 
     last_time = 0
 
-    turns = 0
+    moves = 0
 
     paused = False
     crashed = False
@@ -32,9 +38,9 @@ def play(_snake):
         display.fill((0, 38, 76))
 
         for i in _snake.tail:
-            display.blit(body_img, i * 32)
-        display.blit(head_img, _snake.position * 32)
-        display.blit(food_img, _snake.food * 32)
+            display.blit(body_img, i * 32 * SCALE)
+        display.blit(head_img, _snake.position * 32 * SCALE)
+        display.blit(food_img, _snake.food * 32 * SCALE)
 
         pygame.display.update()
 
@@ -42,7 +48,7 @@ def play(_snake):
             _snake.think()
             _snake.move()
             last_time = 0
-            turns += 1
+            moves += 1
 
         if not _snake.alive:
             crashed = True
@@ -50,10 +56,11 @@ def play(_snake):
         clock.tick(60)
         last_time += clock.get_time()
 
-    print("Snake scored {} points in {} turns".format(_snake.score, turns))
+    print("Snake scored {} points in {} moves".format(_snake.length - 5, moves))
     pygame.quit()
 
 
-snake = AutonomousSnake.load_snake("champions/gen8.pickle")
-print(snake.moves_left)
+snake = AutonomousSnake.load_snake("champions/gen{}.pickle".format(sys.argv[1]))
+# print(snake.moves_left)
+print(snake.velocity)
 play(snake)
